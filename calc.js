ce7df1cbe9becec6e513a42398097fe952ab0e10
Seth21887 <101611145+Seth21887@users.noreply.github.com>
2022-04-07 13:18:27 -0700

    calc adjustments

--- calc.js
+++ calc.js
@@ -1,6 +1,7 @@
 function calculator(){
     console.log("Calculating...")
-    let num1 = Number(prompt("Enter the number")); //number is a keyword, parse into a number.
+    let num1 = Number(prompt("Enter the first number")); //number is a keyword, parse into a number.
+    let num2 = Number(prompt("Enter the second number"));
     document.getElementById("calculationZone").innerHTML=`
     <p>${num1}*10 = ${num1*10}</p>`;
 }
