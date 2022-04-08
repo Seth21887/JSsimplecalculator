8ff2f4612f080916d6400a5d1ab909fa3ad4ae08
Seth21887 <101611145+Seth21887@users.noreply.github.com>
2022-04-08 15:31:19 -0700

    adjustments

--- calc.js
+++ calc.js
@@ -3,5 +3,8 @@ function calculator(){
     let num1 = Number(prompt("Enter the first number")); //number is a keyword, parse into a number.
     let num2 = Number(prompt("Enter the second number"));
     document.getElementById("calculationZone").innerHTML=`
-    <p>${num1}*10 = ${num1*10}</p>`;
+    <p>${num1} * ${num2} = ${num1*num2}</p>
+    <p>${num1} - ${num2} = ${num1-num2}</p>
+    <p>${num1} + ${num2} = ${num1+num2}</p>
+    <p>${num1} / ${num2} = ${num1/num2}</p>`
 }
